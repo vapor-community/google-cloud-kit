79d871cced798d2f93f71c19672e57abd2fc16a1
Adir Burke @ Work <zaraserver@dev.local>
2020-01-18 22:06:36 +1100

    Updating Core to allow for sub in the JWT to work with Gmail API

--- Core/Sources/Configuration/GoogleCloudError.swift
+++ Core/Sources/Configuration/GoogleCloudError.swift
@@ -33,3 +33,43 @@ enum OauthRefreshError: GoogleCloudError {
         }
     }
 }
+
+
+
+public struct GoogleCloudAPIErrorMain: GoogleCloudError, GoogleCloudModel {
+    /// A container for the error information.
+    public var error: GoogleCloudAPIErrorBody
+    
+    public init (error : GoogleCloudAPIErrorBody ) {
+        self.error = error
+    }
+}
+
+public struct GoogleCloudAPIErrorBody: Codable {
+    /// A container for the error details.
+    public var errors: [GoogleCloudAPIError]
+    /// An HTTP status code value, without the textual description.
+    public var code: Int
+    /// Description of the error. Same as `errors.message`.
+    public var message: String
+    
+    public init (errors: [GoogleCloudAPIError], code: Int, message: String) {
+        self.errors = errors
+        self.code = code
+        self.message = message
+    }
+}
+
+public struct GoogleCloudAPIError: Codable {
+    /// The scope of the error. Example values include: global, push and usageLimits.
+    public var domain: String?
+    /// Example values include invalid, invalidParameter, and required.
+    public var reason: String?
+    /// Description of the error.
+    /// Example values include Invalid argument, Login required, and Required parameter: project.
+    public var message: String?
+    /// The location or part of the request that caused the error. Use with location to pinpoint the error. For example, if you specify an invalid value for a parameter, the locationType will be parameter and the location will be the name of the parameter.
+    public var locationType: String?
+    /// The specific item within the locationType that caused the error. For example, if you specify an invalid value for a parameter, the location will be the name of the parameter.
+    public var location: String?
+}
