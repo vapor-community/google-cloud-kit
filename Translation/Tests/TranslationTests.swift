8228cd2dcfbf4c3c2bcba626801e2c422274ea28
Pau Ballart <pballart@gmail.com>
2021-05-04 22:25:40 +0200

    Comment Translate test

--- Translation/Tests/TranslationTests.swift
+++ Translation/Tests/TranslationTests.swift
@@ -16,25 +16,25 @@ class TranslationTests: XCTestCase {
     }
 
     //Just for manual testing purposes.
-    func testAPI() {
-        do {
-            let credentialsConfig = try GoogleCloudCredentialsConfiguration()
-            let translationConfig = GoogleCloudTranslationConfiguration.default()
-
-            let gct = try GoogleCloudTranslationClient(credentials: credentialsConfig,
-                                                       config: translationConfig,
-                                                       httpClient: client,
-                                                       eventLoop: elg.next())
-
-            let translation = gct.translation.translate(text: "Is this working?.", target: "es")
-
-            let content = try translation.wait()
-
-            print(content.data.translations.first!.translatedText)
-            XCTAssertEqual(content.data.translations.count, 1)
-        }
-        catch {
-            XCTFail(error.localizedDescription)
-        }
-    }
+//    func testAPI() {
+//        do {
+//            let credentialsConfig = try GoogleCloudCredentialsConfiguration()
+//            let translationConfig = GoogleCloudTranslationConfiguration.default()
+//
+//            let gct = try GoogleCloudTranslationClient(credentials: credentialsConfig,
+//                                                       config: translationConfig,
+//                                                       httpClient: client,
+//                                                       eventLoop: elg.next())
+//
+//            let translation = gct.translation.translate(text: "Is this working?.", target: "es")
+//
+//            let content = try translation.wait()
+//
+//            print(content.data.translations.first!.translatedText)
+//            XCTAssertEqual(content.data.translations.count, 1)
+//        }
+//        catch {
+//            XCTFail(error.localizedDescription)
+//        }
+//    }
 }
