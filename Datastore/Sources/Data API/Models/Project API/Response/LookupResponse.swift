d968ed7bbfb221424912bcb657a28b8223d5aa2f
Owain Hunt <owain.hunt@johnlewis.co.uk>
2024-08-13 14:22:52 +0100

    Fix LookupResponse so missing entities can be correctly decoded

--- Datastore/Sources/Data API/Models/Project API/Response/LookupResponse.swift
+++ Datastore/Sources/Data API/Models/Project API/Response/LookupResponse.swift
@@ -1,7 +1,11 @@
 import Core
 
 public struct LookupResponse: GoogleCloudModel {
-    public init(deferred: [Key]? = nil, found: [EntityResult]? = nil, missing: [EntityResult]? = nil) {
+    public init(
+        deferred: [Key]? = nil,
+        found: [EntityResult]? = nil,
+        missing: [MissingEntityResult]? = nil
+    ) {
         self.deferred = deferred
         self.found = found
         self.missing = missing
@@ -11,5 +15,16 @@ public struct LookupResponse: GoogleCloudModel {
     /// Entities found as ResultType.FULL entities. The order of results in this field is undefined and has no relation to the order of the keys in the input.
     public let found: [EntityResult]?
     /// Entities not found as ResultType.KEY_ONLY entities. The order of results in this field is undefined and has no relation to the order of the keys in the input.
-    public let missing: [EntityResult]?
+    public let missing: [MissingEntityResult]?
+}
+
+public struct MissingEntityResult: GoogleCloudModel {
+    /// A KEY_ONLY entity
+    public let entity: MissingEntity
+    /// the version of the snapshot that was used to look up the entity
+    public let version: String
+    
+    public struct MissingEntity: GoogleCloudModel {
+        public let key: Key
+    }
 }
